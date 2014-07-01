7095a8521ebb615711bb0e2e60e8f05725df9bf3
imperugo <imperugo@gmail.com>
2014-07-01 22:00:58 +0200

    added express-email

--- src/config/express.js
+++ src/config/express.js
@@ -3,43 +3,62 @@
     var logger = require("../utils/logger");
 
     var path = require('path');
-	var expressValidator = require('express-validator');
+    var expressValidator = require('express-validator');
+    var mailer = require('express-mailer');
 
     expressConfig.init = function(app, express) {
 
-	    //setup view engine
-	    logger.debug("Setting 'Vash' as view engine");
-	    app.set("view engine", "vash");
+        //setup view engine
+        logger.debug("Setting 'Vash' as view engine");
+        app.set("view engine", "vash");
 
-	    logger.debug("Setting 'Views' folder");
-	    var viewsFolder = path.dirname(module.parent.filename) + '/views';
-	    app.set('views', viewsFolder);
+        logger.debug("Setting 'Views' folder");
+        var viewsFolder = path.dirname(module.parent.filename) + '/views';
+        app.set('views', viewsFolder);
 
-	    logger.debug("Enabling GZip compression.");
-	    var compression = require('compression');
-	    app.use(compression({
-	      threshold: 512
-	    }));
+        logger.debug("Enabling GZip compression.");
+        var compression = require('compression');
+        app.use(compression({
+            threshold: 512
+        }));
 
-	    logger.debug("Setting 'Public' folder with caching maxAge: 1 Day.");
-	    var publicFolder = path.dirname(module.parent.filename)  + "/public";
-	    var oneYear = 31557600000;
-	    app.use(express.static(publicFolder, { maxAge: oneYear }));
+        logger.debug("Setting 'Public' folder with caching maxAge: 1 Day.");
+        var publicFolder = path.dirname(module.parent.filename) + "/public";
+        var oneYear = 31557600000;
+        app.use(express.static(publicFolder, {
+            maxAge: oneYear
+        }));
 
-	    logger.debug("Setting parse urlencoded request bodies into req.body.");
-	    var bodyParser = require('body-parser');
-	    app.use(bodyParser.json());
-	    app.use(bodyParser.urlencoded({
-		  extended: true
-		}));
+        logger.debug("Setting parse urlencoded request bodies into req.body.");
+        var bodyParser = require('body-parser');
+        app.use(bodyParser.json());
+        app.use(bodyParser.urlencoded({
+            extended: true
+        }));
 
-		logger.debug("Enable validation....");
-		app.use(expressValidator());
+        logger.debug("Enable validation....");
+        app.use(expressValidator());
 
-	    logger.debug("Overriding 'Express' logger");
-    	app.use(require('morgan')({ "stream": logger.stream }));
+        logger.debug("Overriding 'Express' logger");
+        app.use(require('morgan')({
+            "stream": logger.stream
+        }));
 
-    	require("../controllers/").init(app);
+        mailer.extend(app, {
+            from: 'no-reply@example.com',
+            host: 'smtp.gmail.com', // hostname
+            secureConnection: true, // use SSL
+            port: 465, // port for secure SMTP
+            transportMethod: 'SMTP', // default is SMTP. Accepts anything that nodemailer accepts
+            auth: {
+                user: 'gmail.user@gmail.com',
+                pass: 'userpass'
+            }
+        });
+
+        require("../controllers/").init(app);
+
+
     };
 
 })(module.exports);
--- src/controllers/notify.js
+++ src/controllers/notify.js
@@ -1,5 +1,5 @@
 (function(notifyController) {
-	var logger = require('../utils/logger');
+    var logger = require('../utils/logger');
 
     notifyController.init = function(app) {
         app.post('/api/notify/join', function(req, res) {
@@ -10,31 +10,45 @@
             var errors = req.validationErrors();
             var mappedErrors = req.validationErrors(true);
 
-            if(errors) {
-				logger.warn("Wrong request: ", errors);
-				res.json(400,errors);
-			}
+            if (errors) {
+                logger.warn("Wrong request: ", errors);
+                res.json(400, errors);
+            }
 
-			//here need to implement mail-chimp integration
+            //here need to implement mail-chimp integration
         });
 
 
         app.post('/api/contact/', function(req, res) {
 
-        	req.assert('name', 'Field required').notEmpty();
-        	req.assert('message', 'Field required').notEmpty();
+            req.assert('name', 'Field required').notEmpty();
+            req.assert('message', 'Field required').notEmpty();
             req.assert('email', 'Field required').notEmpty();
             req.assert('email', 'Invalid email format').isEmail();
 
             var errors = req.validationErrors();
             var mappedErrors = req.validationErrors(true);
 
-            if(errors) {
-				logger.warn("Wrong request: ", errors);
-				res.json(400,errors);
-			}
+            if (errors) {
+                logger.warn("Wrong request: ", errors);
+                res.json(400, errors);
+            }
 
-			//here the code to send the email, and response with the right status code and info
+            app.mailer.send('email/contact', { //Template (it uses the same engine of express)
+                to: 'example@example.com', // REQUIRED. This can be a comma delimited string just like a normal email to field. 
+                subject: 'Test Email', // REQUIRED.
+                otherProperty: 'Other Property' // All additional properties are also passed to the template as local variables.
+            }, function(err) {
+                if (err) {
+                    // handle error
+                    console.log(err);
+                    res.send('There was an error sending the email');
+                    return;
+                }
+                res.send('Email Sent');
+            });
+
+            //here the code to send the email, and response with the right status code and info
         });
     };
 
