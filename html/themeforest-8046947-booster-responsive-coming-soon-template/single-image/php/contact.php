bd76bbc982cd11919ac2f79e3761f621d4baa83f
Daniel Crisp <daniel@danielcrisp.com>
2014-07-02 13:32:50 +0100

    Added comments for Ugo

--- html/themeforest-8046947-booster-responsive-coming-soon-template/single-image/php/contact.php
+++ html/themeforest-8046947-booster-responsive-coming-soon-template/single-image/php/contact.php
@@ -1,5 +1,8 @@
 <?php
 
+/*
+ *	Check if there are any POST values, if not exit the script
+ */
 if(!$_POST) exit;
 
 // Email address verification, do not edit.
@@ -7,8 +10,17 @@ function isEmail($email) {
 	return(preg_match("/^[-_.[:alnum:]]+@((([[:alnum:]]|[[:alnum:]][[:alnum:]-]*[[:alnum:]])\.)+(ad|ae|aero|af|ag|ai|al|am|an|ao|aq|ar|arpa|as|at|au|aw|az|ba|bb|bd|be|bf|bg|bh|bi|biz|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|com|coop|cr|cs|cu|cv|cx|cy|cz|de|dj|dk|dm|do|dz|ec|edu|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gh|gi|gl|gm|gn|gov|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|in|info|int|io|iq|ir|is|it|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|mg|mh|mil|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|museum|mv|mw|mx|my|mz|na|name|nc|ne|net|nf|ng|ni|nl|no|np|nr|nt|nu|nz|om|org|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|pro|ps|pt|pw|py|qa|re|ro|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|sk|sl|sm|sn|so|sr|st|su|sv|sy|sz|tc|td|tf|tg|th|tj|tk|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|um|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)$|(([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5])\.){3}([0-9][0-9]?|[0-1][0-9][0-9]|[2][0-4][0-9]|[2][5][0-5]))$/i",$email));
 }
 
+
+/*
+ * HANDLE CROSS SYSTEM DIFFERENCES IN THE NEWLINE CHARACTOR
+ */
 if (!defined("PHP_EOL")) define("PHP_EOL", "\r\n");
 
+/*
+ *	Assign POST variables to local variables
+ *
+ *	$_POST is basically a global associative array that contains all the POST values. $_GET contains GET values
+ */
 $name     = $_POST['name'];
 $email    = $_POST['email'];
 $phone   = $_POST['phone'];
@@ -16,8 +28,19 @@ $subject  = $_POST['subject'];
 $comments = $_POST['comments'];
 $verify   = $_POST['verify'];
 
+/*
+ *	trim() removes leading and trailing whitespace from a value
+ *
+ *	If the value is '' after trim then show an error message
+ */
 if(trim($name) == '') {
+	/*
+	 *	echo prints a value to the page
+	 */
 	echo '<div class="error_message">Attention! You must enter your name.</div>';
+	/*
+	 *	exit() means stop the script
+	 */
 	exit();
 } else if(trim($email) == '') {
 	echo '<div class="error_message">Attention! Please enter a valid email address.</div>';
@@ -27,12 +50,18 @@ if(trim($name) == '') {
 	echo '<div class="error_message">Attention! Please enter your message.</div>';
 	exit();
 }
+/*
+ *	Check if the email address provided is NOT valid (uses isEmail function defined above)
+ */
 else if(!isEmail($email)) {
 	echo '<div class="error_message">Attention! You have entered an invalid e-mail address. Please try again.</div>';
 	exit();
 }
 
-
+/* 
+ *	Check if PHP's magic quotes feature is enabled
+ *	If it is strip slashes from the value
+ */
 
 if(get_magic_quotes_gpc()) {
 	$comments = stripslashes($comments);
@@ -63,13 +92,25 @@ $e_body = "You have been contacted by $name with regards to $subject, their addi
 $e_content = "\"$comments\"" . PHP_EOL . PHP_EOL;
 $e_reply = "You can contact $name via email, $email or via phone $phone";
 
+/*
+ *	Wrap the string to 70 characters
+ */
 $msg = wordwrap( $e_body . $e_content . $e_reply, 70 );
+
+/*
+ *	Set the email headers
+ */
 
 $headers = "From: $email" . PHP_EOL;
 $headers .= "Reply-To: $email" . PHP_EOL;
 $headers .= "MIME-Version: 1.0" . PHP_EOL;
 $headers .= "Content-type: text/plain; charset=utf-8" . PHP_EOL;
 $headers .= "Content-Transfer-Encoding: quoted-printable" . PHP_EOL;
+
+/*
+ *	Attempt to send the email using PHP's built in mail() function 
+ *	If attempt fails it will use the 'else'
+ */
 
 if(mail($address, $e_subject, $msg, $headers)) {
 
